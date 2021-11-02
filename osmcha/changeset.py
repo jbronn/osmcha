86ef597785c110651bcfc0bb7aa27a1504454255
Justin Bronn <justin.bronn@maxar.com>
2021-11-02 11:31:26 -0600

    Use `OSMCHA_OSM_BASE_URL` for variable name, as it's possible we want it different from `OSM_BASE_URL` used by Django.

--- osmcha/changeset.py
+++ osmcha/changeset.py
@@ -30,13 +30,13 @@ except TypeError:
         failobj=join(dirname(abspath(__file__)), 'suspect_words.yaml')
         )
 WORDS = yaml.safe_load(open(SUSPECT_WORDS_FILE, 'r').read())
-OSM_BASE_URL = environ.get(
-    'OSM_BASE_URL',
+OSMCHA_OSM_BASE_URL = environ.get(
+    'OSMCHA_OSM_BASE_URL',
     'https://www.openstreetmap.org'
     )
 OSM_USERS_API = environ.get(
     'OSM_USERS_API',
-    '{}/api/0.6/user/{{user_id}}'.format(OSM_BASE_URL)
+    '{}/api/0.6/user/{{user_id}}'.format(OSMCHA_OSM_BASE_URL)
     )
 # infosrmation that we get from changeset xml key
 MANDATORY_TAGS = ['id', 'user', 'uid', 'bbox', 'created_at', 'comments_count']
@@ -102,7 +102,7 @@ def get_changeset(changeset):
         changeset: the id of the changeset.
     """
     url = '{}/api/0.6/changeset/{}/download'.format(
-        OSM_BASE_URL,
+        OSMCHA_OSM_BASE_URL,
         changeset
         )
     return ET.fromstring(requests.get(url).content)
@@ -115,7 +115,7 @@ def get_metadata(changeset):
     Args:
         changeset: the id of the changeset.
     """
-    url = '{}/api/0.6/changeset/{}'.format(OSM_BASE_URL, changeset)
+    url = '{}/api/0.6/changeset/{}'.format(OSMCHA_OSM_BASE_URL, changeset)
     return ET.fromstring(requests.get(url).content)[0]
 
 
@@ -379,7 +379,7 @@ class Analyse(object):
 
             if 'iD' in self.editor:
                 trusted_hosts = [
-                    OSM_BASE_URL.split('://')[-1].split('/')[0],
+                    OSMCHA_OSM_BASE_URL.split('://')[-1].split('/')[0],
                     'improveosm.org',
                     'strava.github.io',
                     'preview.ideditor.com',
